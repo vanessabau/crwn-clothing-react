febab57acff3f9754224d4ef1d500f6e3f1fd198
Vanessa Bautista <vanessawebdev@gmail.com>
2021-11-09 12:28:59 -0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Router>
     </div>
   );
 }

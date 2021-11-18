22a7107650e7b4ce9cc164cc922301fcf278d57b
Vanessa Bautista <vanessawebdev@gmail.com>
2021-11-18 08:26:15 -0800

    added sign in page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shop.component.jsx";
 import Header from "./components/header/header.component.jsx";
+import HomePage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Router>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Router>
     </div>
   );

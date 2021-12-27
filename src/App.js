bc0c784cafcee135e9ee6442e1ae54b10370f60a
Vanessa Bautista <vanessawebdev@gmail.com>
2021-12-27 13:54:38 -0800

    updated props destructuring

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends React.Component {
   componentDidMount() {
     console.log(this.props);
     const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);

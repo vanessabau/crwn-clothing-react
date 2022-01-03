c4139745a8568f5281130957bd252bce64b4de89
Vanessa Bautista <vanessawebdev@gmail.com>
2022-01-03 07:37:39 -0800

    added a collection page and route

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./collection.styles.scss";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+const CollectionPage = () => (
+  <div className="collection">
+    <h2>Collection Page</h2>
+  </div>
+);
+
+export default CollectionPage;

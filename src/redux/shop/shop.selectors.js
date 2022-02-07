80d4b29d5188abc1658f539042c8eda701d0a5c3
Vanessa Bautista <vanessawebdev@gmail.com>
2022-02-07 07:05:59 -0800

    added commentary

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -9,6 +9,7 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
+  // get all the keys, map over the array of keys and get the value of the collections object at that key which gives us an array of the items we ae trying to get
   collections => Object.keys(collections).map(key => collections[key])
 );
 
